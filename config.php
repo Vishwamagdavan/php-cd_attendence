a0cc33e62af75b706b94b32fd938a11fa88e451e
Chronicle Manager <production@chroniclemanager.com>
2018-11-09 07:50:38 +0530

    local commit

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("127.0.0.1:3306","root","root","attendance") or die("error")
+$con=mysqli_connect("127.0.0.1:3306","root","admin@cmanager","epushserver") or die("error")
 ?>
